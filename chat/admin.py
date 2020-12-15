c77d93634e299aefc195ea887aae78a8f7a10aad
mani-barathi <manibarathi.s@gmail.com>
2020-12-15 22:58:31 +0530

    registered model to admin page

--- chat/admin.py
+++ chat/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Message
 # Register your models here.
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_per_page = 50
+    ordering = ['-timestamp']
+    # search_fields = ['user']
+
+admin.site.register(Message,MessageAdmin)
